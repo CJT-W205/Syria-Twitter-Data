f1d000ff95c56c12c33f71d22ee5bb750fa43ac5
Janak Mayer <janak.mayer@gmail.com>
2015-02-25 19:52:21 -0500

    Eliminated redundant second API call and second mongo collection

--- acquisition/crawl_users.py
+++ acquisition/crawl_users.py
@@ -25,7 +25,6 @@ class FriendCrawler(object):
         conn = pymongo.MongoClient()
         db = conn.network
         self.users = db.user_profiles
-        self.following = db.user_following
 
     def crawl(self, user_id, current_depth=0):
         self.log('current depth: %d, max depth: %d' % (current_depth, self.max_depth))
@@ -43,21 +42,12 @@ class FriendCrawler(object):
         if not user:
             user = self.get_user_from_api(user_id)
             if not user:
-                return self.crawled_list # Couldn't get user so exit recursive call
+                return self.crawled_list  # Couldn't get user so exit recursive call
             self.write_user_to_mongo(user)
 
         screen_name = self.encode_str(user['screen_name'])
-        following = self.get_following_from_mongo(user_id)
-        if not following:
-            self.log('No data following for screen name "%s"' % screen_name)
-            following_ids = self.get_following_from_api(user_id, user)
-        else:
-            following = following.get('following', None)
-            if not following:
-                self.log('ERROR RETRIEVING FRIENDS FOR SCREEN NAME "%s"' % screen_name)
-                return self.crawled_list  # @TODO Double check - this should never be triggered
-            following_ids = [followed['following_id'] for followed in following]
-        self.log('Found %d following for %s' % (len(following_ids), screen_name))
+        following_ids = self.get_following_from_mongo(user_id)
+
         cd = current_depth
         if cd+1 < self.max_depth:
             for fid in following_ids[:FRIENDS_OF_FRIENDS_LIMIT]:
@@ -76,62 +66,34 @@ class FriendCrawler(object):
         self.users.insert(user)
 
     def get_user_from_api(self, user_id):
-        user = None
+        userDict = None
         try:
             user = self.api.get_user(user_id)
             if not getattr(user, 'screen_name', None):
                 return None
-            user = {'_id': user.id,
+            userDict = {'_id': user.id,
                     'name': user.name,
                     'screen_name': user.screen_name,
                     'following_count': user.friends_count,
                     'followers_count': user.followers_count,
+                    'following_ids': user._api.followers_ids(user_id=user.id),
                     'followers_ids': user.followers_ids(),
                     'location': user.location,
                     'time_zone': user.time_zone,
                     'created_at': datetime.datetime.strftime(user.created_at, '%Y-%h-%m %H:%M')}
+            self.log('%s is following %s others' % (user.screen_name, user.friends_count))
         except tweepy.TweepError, error:
                     if str(error) == 'Not authorized.':
                         self.log('Can''t access user data - not authorized.')
                     if str(error) == 'User has been suspended.':
                         self.log('User suspended.')
                     self.log(error[0][0])
-        return user
+        return userDict
 
     def get_following_from_mongo(self, user_id):
-        following = self.following.find_one({'_id': user_id})
+        user = self.users.find_one({'_id': user_id})
+        following = user.get('following_ids', None)
         return following
-
-    def get_following_from_api(self, user_id, user):
-        following_ids = []
-        self.write_following_to_mongo(user_id, user)
-        self.log('Retrieving following list for user "%s"' % user['screen_name'])
-        c = tweepy.Cursor(api.friends, id=user['_id'], count=self.cursor_count).items()
-
-        following_count = 0
-        while True:
-            try:
-                followed = c.next()
-                following_ids.append(followed.id)
-                self.update_following_in_mongo(followed, user_id)
-                following_count += 1
-                if following_count >= MAX_FOLLOWING:
-                    self.log('Reached max length of following list for "%s".' % user['screen_name'])
-                    break
-            except StopIteration:
-                break
-        return following_ids
-
-    def write_following_to_mongo(self, user_id, user):
-        d = {'_id': user_id,
-             'screen_name': user['screen_name']}
-        self.following.insert(d)
-
-    def update_following_in_mongo(self, followed, user_id):
-        f = {'following_id': followed.id,
-             'following_sname': self.encode_str(followed.screen_name),
-             'following_name': self.encode_str(followed.name)}
-        self.following.update({"_id": user_id}, {"$push": {'following': f}})
 
     def log(self, message):
         if self.verbose:
--- analysis/simple_query.py
+++ analysis/simple_query.py
@@ -1,11 +1,10 @@
 import pymongo
 
 db = pymongo.MongoClient().network
+db.connection.drop_database('network')
+
+
 # results = db.user_followers.find()
 #
 # for user in results:
 #     print user
-
-# db.connection.drop_database('test')
-# db.connection.drop_database('user_profiles')
-db.connection.drop_database('network')
