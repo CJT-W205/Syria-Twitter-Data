46d4d73fcdffd466532a993596d191216b681df1
Janak Mayer <janak.mayer@gmail.com>
2015-04-22 21:33:23 -0400

    API ready for integration with mongo and real data!

--- analysis/sample_transform.py
+++ analysis/sample_transform.py
@@ -14,12 +14,15 @@ nodes = []
 edges = []
 counter = 0
 
+colors = ['#e41a1c', '#377eb8', '#4daf4a', '#984ea3']
+
 for node in old_nodes:
     nodes.append({
         'group': node['group'],
         'id': str(node['name']),
-        'x': random.randint(0, 10000),
-        'y': random.randint(0, 10000),
+        'x': random.randint(0, 10000),  # To be calculated in the API based on cluster, and filter options
+        'y': random.randint(0, 10000),  # To be calculated in the API end based on cluster, and filter options
+        'color': colors[random.randint(0, 3)],  # Pick from colors, based on cluster (just random right now)
         'size': 0.1,
         'label': 'screen name: %s | isis group: %s | following count: %s' % (users_dict[str(node['name'])]['screen_name'],
                                                                              users_dict[str(node['name'])]['isis'],
